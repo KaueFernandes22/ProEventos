aa958b10443d8c66cb32f3ca5a61873343737897
@kaue_fernandes <kaue.fernandes@smartgo.com.br>
2022-11-20 15:14:33 -0300

    Att 2

--- back/src/ProEventos.API/Controllers/EventoController.cs
+++ back/src/ProEventos.API/Controllers/EventoController.cs
@@ -4,7 +4,8 @@ using System.Linq;
 using System.Threading.Tasks;
 using Microsoft.AspNetCore.Mvc;
 using Microsoft.Extensions.Logging;
-using ProEventos.API.Models; 
+using ProEventos.API.Data;
+using ProEventos.API.Models;
 
 namespace ProEventos.API.Controllers
 {
@@ -12,42 +13,24 @@ namespace ProEventos.API.Controllers
     [Route("api/[controller]")]
     public class EventoController : ControllerBase
     {
-        public EventoController()
+        private readonly DataContext context;
+        public EventoController(DataContext context)
         {
-
+            this.context = context;
         }
 
-        public IEnumerable<Evento> evento = new Evento []{
-                new Evento() {
-                    EventoID = 1,
-                    Tema = "Angular 11 e .NET 5",
-                    Local = "Curitiba",
-                    DataEvento = DateTime.Now.AddDays(2).ToString("dd/MM/yyy"),
-                    QtdPessoas = 250,
-                    Lote = "1 Lote",
-                    ImagemURL = "foto.png",
-                },
-                new Evento() {
-                    EventoID = 2,
-                    Tema = "Como treinar seu dragão",
-                    Local = "São Paulo",
-                    DataEvento = DateTime.Now.AddDays(3).ToString("dd/MM/yyy"),
-                    QtdPessoas = 450,
-                    Lote = "3 Lote",
-                    ImagemURL = "foto2.png",
-                }
-            };
+        public IEnumerable<Evento> evento = new Evento[]{};
 
         [HttpGet]
         public IEnumerable<Evento> Get()
         {
-            return evento;
+            return context.Eventos;
         }
 
         [HttpGet("{id}")]
-        public IEnumerable<Evento> GetById( int id)
+        public IEnumerable<Evento> GetById(int id)
         {
-            return evento.Where(evento => evento.EventoID == id);
+            return context.Eventos.Where(evento => evento.EventoID == id);
         }
 
         [HttpPost]
@@ -67,6 +50,6 @@ namespace ProEventos.API.Controllers
         {
             return $"Exemplo de Delete com id = {id}";
         }
-        
+
     }
 }
